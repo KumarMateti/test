3913f7141d31dbbd1b3dedffd47f481e2043f798
Kumaraswamy <kumarmateti@gmail.com>
2020-06-20 09:08:23 +0530

    datepicker test

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './App.css'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+function App () {
+  const [selectedDate, setSelectedDate] = useState(null)
+  
+  return (
+    <div className='App'>
+      <DatePicker
+        selected={selectedDate}
+        onChange={date => setSelectedDate(date)}
+        onkeypress="this."
+        placeholderText={'dd/mm/yyyy'}
+        // minDate={new Date()}
+        maxDate={new Date()}
+        //filterDate={date => date.getDay() !== 6 && date.getDay() !== 0}
+        showMonthDropdown
+        showYearDropdown
+        scrollableYearDropdown
+      />
+    </div>
+  )
+}
+
+export default App
